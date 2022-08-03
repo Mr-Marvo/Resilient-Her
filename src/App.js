6b9974b78db1aa535fe2a34d0d1d5283dca32462
himal999 <himal.official99@gmail.com>
2022-08-03 11:43:07 +0530

    mouse move effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Desc from "./components/desc/Desc";
 import Detail from "./components/detail/Detail";
 import Hero from "./components/hero/Hero";
-
+import React from 'react'
 
 
 function App() {
+  
   return (
-  <div>
+    <div>
+   
     <Hero/>
     <Desc/>
     <Detail/>
--- src/components/team/TeamMember.jsx
+++ src/components/team/TeamMember.jsx
@@ -7,12 +7,12 @@ const TeamMember = () => {
     <div className=' flex flex-col w-[210px] h-[330px]   relative'>
         <div className=' flex w-full flex-col h-auto container mx-auto'>
             <div className=' flex flex-col w-[210px] h-[190px]  bg-blue-700 border-[2px] border-white relative'>
-                <img src={require("../../assets/images/slide.webp")} alt='team' className=' -top-10  absolute bg-cover flex  w-full h-[225px]'/>
+                <img src={require("../../assets/images/slide.webp")} alt='team' className=' -top-10  absolute bg-cover flex  w-full h-[225px] z-40'/>
             </div>
             <div className=' flex flex-col w-full h-auto justify-center items-center mt-4'>
-                <h3 className=' text-white text-2xl  font-bold  font-sourceCode'>Sameer Baloch</h3>
-                <p className=' text-white text-center mt-2'>Artistic Director and creator of the Llamas</p>
-                <div className=' flex flex-row  mt-2 text-white text-xl'>
+                <h3 className=' text-white text-2xl  font-bold  font-sourceCode z-40'>Sameer Baloch</h3>
+                <p className=' text-white text-center mt-2 z-40'>Artistic Director and creator of the Llamas</p>
+                <div className=' flex flex-row  mt-2 text-white text-xl z-40'>
                     <FiTwitter className=' mx-4'/>
                     <AiOutlineInstagram/>
                 </div>
