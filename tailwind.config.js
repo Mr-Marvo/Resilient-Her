2a5252c6f4d50fc7e9f35f9b2018193ee0e89c82
himal999 <himal.official99@gmail.com>
2022-08-24 06:31:39 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
-      'xxs':'320px',
-      'xs':'360px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      xxs: '320px',
+      xs: '360px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
-      '3xl':'2500px'
-     },
+      '3xl': '2500px',
+    },
     extend: {
-      fontFamily:{
-        custome: ["NeueKabelW01","Regular"]
+      fontFamily: {
+        custome: ['NeueKabelW01', 'Aquatico', 'Regular'],
       },
-      colors:{
-          puprpule_1:"rgb(213, 184, 255)",
-          dark_1:"rgba(0,0,0,0.6)",
-          light_1:"rgba(255,255,255,1)",
-          xx:"#161922"
-      }
+      colors: {
+        puprpule_1: 'rgb(213, 184, 255)',
+        dark_1: 'rgba(0,0,0,0.6)',
+        light_1: 'rgba(255,255,255,1)',
+        xx: '#161922',
+      },
     },
   },
   plugins: [],
-}
+};
