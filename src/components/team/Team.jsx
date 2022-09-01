9df28e92138fbb2bd5826b2b482019cca1d57bb9
Nishan Premakumara <aknpremakumara@gmail.com>
2022-09-01 10:00:52 +0530

    Quote Update

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -937,10 +937,10 @@ const Team = () => {
           />
         </div>
       </div>
-      <div className="text-white xxs:ml-4 sm:ml-0  flex flex-row  z-40 justify-center items-center">
+      <div className="text-white xxs:ml-4 sm:ml-0 flex flex-row z-40 justify-center items-center">
         <figure>
           <blockquote cite="">
-            <p className="  font-medium tracking-tighter  italic  text-center">
+            <p className="text-xl font-medium tracking-tighter italic text-center">
               <strong className=" text-xl tracking-tighter">"</strong>Some women fear the fire
               some women simply become it..."
             </p>
