a41a7d2e3e4600a599130ee04b464abb3ffd3080
Nishan Premakumara <aknpremakumara@gmail.com>
2022-09-03 17:25:28 +0530

    Contract Update

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
     title: 'RESILENT HER',
     description: 'A collection of 9880 iron-willed females with one heart and one fight. The future is female.',
-    contractAddress: '0xc3cDdC6269Df953A7f8830894b793b4dbD5DE104',
+    contractAddress: '0x777e18062121E0792AF99D3606b9D56EE0E59f0c',
     maxMintAmount: 1,
     presaleMaxMintAmount: 2,
     price: 0.01
