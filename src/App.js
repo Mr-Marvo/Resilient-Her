4bb8a389cf4bb000704b07b29616954835eb7790
himal999 <himal.official99@gmail.com>
2022-09-07 16:18:17 +0530

    added new page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-
-import Hero from "./components/hero/Hero";
-import React,{Routes,Route} from "react-router-dom";
-import Desc from "./components/desc/Desc";
-import Roadmap from "./components/roadmap/Roadmap";
-import Faq from "./components/faq/Faq";
-import Team from "./components/team/Team";
-import AboutNFT from "./components/aboutNFT/AboutNFT";
-import Tearm from "./components/tearm&Condition/Tearm";
-import Policy from "./components/tearm&Condition/Policy";
-import NFTLicence from "./components/tearm&Condition/NFTLicence";
-import Mint from "./components/mint/Mint";
-
-
+import Hero from './components/hero/Hero';
+import React, { Routes, Route } from 'react-router-dom';
+import Desc from './components/desc/Desc';
+import Roadmap from './components/roadmap/Roadmap';
+import Faq from './components/faq/Faq';
+import Team from './components/team/Team';
+import AboutNFT from './components/aboutNFT/AboutNFT';
+import Future from './components/future/Future';
+import Tearm from './components/tearm&Condition/Tearm';
+import Policy from './components/tearm&Condition/Policy';
+import NFTLicence from './components/tearm&Condition/NFTLicence';
+import Mint from './components/mint/Mint';
 
 function App() {
-  
   return (
     <div>
-   
-    <Routes>
-      <Route exact path ="/" element={<Hero/>} />
-      <Route exact path ="/about" element={<Desc/>} />
-      <Route exact path ="/aboutNFT" element={<AboutNFT/>} />
-      <Route exact path ="/roadmap" element={<Roadmap/>} />
-      <Route exact path ="/mint" element={<Mint/>} />
-      <Route exact path ="/faq" element={<Faq/>} />
-      <Route exact path ="/team" element={<Team/>} />
-      <Route exact path ="/policy" element={<Policy/>} /> 
-      <Route exact path ="/term" element={<Tearm/>} />
-       <Route exact path ="/nft" element={<NFTLicence/>} />
-    </Routes>
-  
-  </div>
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/about" element={<Desc />} />
+        <Route exact path="/aboutNFT" element={<AboutNFT />} />
+        <Route exact path="/future" element={<Future />} />
+        <Route exact path="/roadmap" element={<Roadmap />} />
+        <Route exact path="/mint" element={<Mint />} />
+        <Route exact path="/faq" element={<Faq />} />
+        <Route exact path="/team" element={<Team />} />
+        <Route exact path="/policy" element={<Policy />} />
+        <Route exact path="/term" element={<Tearm />} />
+        <Route exact path="/nft" element={<NFTLicence />} />
+      </Routes>
+    </div>
   );
 }
 
