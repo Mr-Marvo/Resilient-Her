d6c9ce1ac80d5e58850a1cd667b00663dc88764a
Thanura Nadun <nadunnew@gmail.com>
2022-09-16 01:14:20 +0530

    new loader

--- src/App.js
+++ src/App.js
@@ -11,47 +11,50 @@ import Policy from './components/tearm&Condition/Policy';
 import NFTLicence from './components/tearm&Condition/NFTLicence';
 import Mint from './components/mint/Mint';
 import { useEffect, useState } from 'react';
-import video from './assets/loader.mp4';
+// import video from './assets/loader.mp4';
 
 function App() {
   const [spiner, setSpiner] = useState(true);
 
+  const loaderObject = document.querySelector('#loader')
+
+   const hideLoader = () => loaderObject.classList.add('hidden')
+
   useEffect(() => {
     setTimeout(() => {
-      setSpiner(false);
+    //   // setSpiner(false);
+    hideLoader()
     }, 2000);
   }, []);
+
   return (
     <div>
-      {spiner ? (
-        <div className=" w-full  h-screen bg-black relative flex flex-col justify-center items-center">
-          <video
-            autoPlay
-            loop
-            muted
-            className=" w-[500px] h-[500px] object-cover "
-          >
-            <source src={video} type="video/mp4" />
-          </video>
-          <div className="loader ">
-            <div className="loaderBar"></div>
-          </div>
+      {/* <div className={ (spiner ?'':'hidden') + ' w-full  h-screen bg-black relative flex flex-col justify-center items-center'}>
+        <video
+          autoPlay
+          loop
+          muted
+          className=" w-[500px] h-[500px] object-cover "
+        >
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="loader ">
+          <div className="loaderBar"></div>
         </div>
-      ) : (
-        <Routes>
-          <Route exact path="/" element={<Hero />} />
-          <Route exact path="/about" element={<Desc />} />
-          <Route exact path="/aboutNFT" element={<AboutNFT />} />
-          <Route exact path="/future" element={<Future />} />
-          <Route exact path="/roadmap" element={<Roadmap />} />
-          <Route exact path="/mint" element={<Mint />} />
-          <Route exact path="/faq" element={<Faq />} />
-          <Route exact path="/team" element={<Team />} />
-          <Route exact path="/policy" element={<Policy />} />
-          <Route exact path="/term" element={<Tearm />} />
-          <Route exact path="/nft" element={<NFTLicence />} />
-        </Routes>
-      )}
+      </div> */}
+      <Routes className={ (spiner ?'hidden':'')} >
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/about" element={<Desc />} />
+        <Route exact path="/aboutNFT" element={<AboutNFT />} />
+        <Route exact path="/future" element={<Future />} />
+        <Route exact path="/roadmap" element={<Roadmap />} />
+        <Route exact path="/mint" element={<Mint />} />
+        <Route exact path="/faq" element={<Faq />} />
+        <Route exact path="/team" element={<Team />} />
+        <Route exact path="/policy" element={<Policy />} />
+        <Route exact path="/term" element={<Tearm />} />
+        <Route exact path="/nft" element={<NFTLicence />} />
+      </Routes>
     </div>
   );
 }
