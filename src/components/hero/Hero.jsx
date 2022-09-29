30e0e65ea4638c356c52a7b5a8040019453de7b9
Nishan Premakumara <aknpremakumara@gmail.com>
2022-09-29 21:55:33 +0530

    New Mint Page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import RippleCanvas from "../rippleEffect/RippleCanvas"
-
 import Navbar from "../navbar/Navbar"
 import "./Hero.css"
 import Footer from "../footer/Footer"
@@ -12,7 +10,7 @@ const Hero = () => {
     <RippleCanvas>
       <Navbar />
       <div className=' flex flex-row   container mx-auto justify-center items-center '>
-        <div className=' flex flex-col w-full justify-center items-center hero-img  absolute xxs:top-0  sm:top-28 left-0  '>
+        <div className=' flex flex-col w-full h-full justify-center items-center hero-img  absolute xxs:top-0  sm:top-28 left-0  '>
           <img src={require("../../assets/images/logo-bg.png")} className=' bg-cover  top-0 left-0 ' alt="hero" />
         </div>
       </div>
--- src/components/mint/Mint.jsx
+++ src/components/mint/Mint.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Footer from '../footer/Footer';
 import Navbar from '../navbar/Navbar';
 import './Mint.css';
-import './base.css';
-import './skeleton.css';
 import RippleCanvas from '../rippleEffect/RippleCanvas';
 
 const Mint = () => {
@@ -12,21 +10,18 @@ const Mint = () => {
     <RippleCanvas>
       <Navbar />
 
-      <div class="outer">
-        <div class="middle">
-          <div class="container">
+      <div className='flex flex-col justify-center items-center relative h-screen -mt-28'>
+        <div className='xs:w-[300px]  md:w-[500px] lg:w-[800px] h-auto border-8 border-x-0 border-solid border-transparent border-t-[#066fb7] border-b-[#066fb7]' >
 
-            <div class="transbox">
-              <h1 class='text'>Launching Soon</h1>
-            </div>
+          <h1 className='text-[#c200c9] text-[40px] font-medium tracking-[10px] leading-10 pt-[20px] pb-[20px] text-center font-mono'>LAUNCHING SOON</h1>
 
-            <p>&copy; Resilient-Her | All Rights Reserved.</p>
-          </div>
         </div>
+        <br />
+        <p className='text-black'>&copy; Resilient-Her | All Rights Reserved.</p>
       </div>
 
       <Footer />
-    </RippleCanvas>
+    </RippleCanvas >
   );
 };
 
